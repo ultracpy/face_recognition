77cc93fa01fbffd754228272e7dbf7e03f29997f
moneye <ultracpy@gmail.com>
2021-08-27 08:36:41 +0800

    1234

--- examples/test.py
+++ examples/test.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 import face_recognition
 import cv2
 import numpy as np
@@ -12,11 +15,11 @@ import numpy as np
 # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-# video_capture = cv2.VideoCapture('rtsp://123:1234qwer@192.168.1.250:554/h265/ch35/main/av_stream')
+# video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture('rtsp://123:1234qwer@192.168.1.250:554/h265/ch35/main/av_stream')
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("1.png")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
@@ -38,13 +41,16 @@ face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
+counter = 0
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    small_frame = frame
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -62,20 +68,28 @@ while True:
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+                localtime = random.choice("abcdefsadsfgpispgipoasidf")
+                print(localtime)
 
             # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+            # face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            # best_match_index = np.argmin(face_distances)
+            # if matches[best_match_index]:
+            #     name = known_face_names[best_match_index]
 
             face_names.append(name)
 
-    process_this_frame = not process_this_frame
+    # process_this_frame = not process_this_frame
 
+    if counter > 30:
+        process_this_frame = True
+        counter = 0
+    else:
+        counter += 1
+        process_this_frame = False
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
